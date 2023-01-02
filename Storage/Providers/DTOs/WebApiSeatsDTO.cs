312c1e11cd696acd385ad77ebe92d7ac01219078
Ville Palo <ville.palo@avanio.com>
2023-01-02 15:57:09 +0200

    hkd-82 fetch seats by meetingId and caseNumber

---
+++ Storage/Providers/DTOs/WebApiSeatsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Storage.Controllers.MeetingInfo.DTOs
+{
+    public class WebApiSeatDTO
+    {
+        public string? PersonFI { get; set; }
+
+        public string? PersonSV { get; set; }
+
+        public string? SeatId { get; set; }
+    }
+}
