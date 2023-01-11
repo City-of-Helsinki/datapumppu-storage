4239cf3bc0155efe54f5d0a6ea91a66e2015c139
vijupa <100693495+vijupa@users.noreply.github.com>
2023-01-11 09:02:03 +0200

    User auth (#20)
    * user authentication
    
    * Added password salt
    
    * user_auth cleanup
    
    * user auth cleanup

---
+++ Storage/Controllers/AuthenticationController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Storage.Repositories;
+using System.Security.Cryptography;
+
+namespace Storage.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthenticationController
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAdminUsersRepository _adminUsersRepository;
+
+        public AuthenticationController(
+            IConfiguration configuration,
+            IAdminUsersRepository adminUsersRepository)
+        {
+            _configuration = configuration;
+            _adminUsersRepository = adminUsersRepository;
+        }
+
+        [HttpGet("validate")]
+        public async Task<IActionResult> IsValidUser(
+            [FromQuery] string username,
+            [FromQuery] string password)
+        {
+            using var md5 = System.Security.Cryptography.MD5.Create();
+            var bytes = System.Text.Encoding.ASCII.GetBytes(_configuration["PASSWORD_SALT"] + password);
+            var passwordHash = Convert.ToBase64String(md5.ComputeHash(bytes));
+
+            var exists = await _adminUsersRepository.UserExists(new Repositories.Models.AdminUser
+            {
+                Username = username,
+                Password = passwordHash
+            });
+
+            return exists ? new OkResult() : new ForbidResult();
+        }
+    }
+}
