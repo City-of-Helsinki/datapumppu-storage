900d4f790cdec8007dedb4c791bb7be5e8661275
vijupa <100693495+vijupa@users.noreply.github.com>
2023-01-16 09:39:51 +0200

    Hkd 85 statements search (#25)
    * statement_video_links added video positions
    
    * video link cleanup
    
    * hkd-85 statements

--- Storage/Providers/StatementProvider.cs
+++ Storage/Providers/StatementProvider.cs
@@ -10,7 +10,9 @@ namespace Storage.Providers
 {
     public interface IStatementProvider
     {
-        Task<List<WebApiStatementsDTO>> GetSpeakingTurns(string meetingId, string caseNumber);
+        Task<List<WebApiStatementsDTO>> GetStatements(string meetingId, string caseNumber);
+
+        Task<List<WebApiStatementsDTO>> GetStatementsByPerson(string name, int year);
     }
 
     public class StatementProvider : IStatementProvider
@@ -28,20 +30,43 @@ namespace Storage.Providers
             _videoSyncRepository = videoSyncRepository;
         }
 
-        public async Task<List<WebApiStatementsDTO>> GetSpeakingTurns(string meetingId, string caseNumber)
+        public async Task<List<WebApiStatementsDTO>> GetStatements(string meetingId, string caseNumber)
         {
-            _logger.LogInformation($"GetSpeakingTurns {meetingId} {caseNumber}");
+            _logger.LogInformation($"GetStatements {meetingId} {caseNumber}");
 
-            var speakingTurns = await _speakingTurnsRepository.GetSpeakingTurns(meetingId, caseNumber);
+            var speakingTurns = await _speakingTurnsRepository.GetStatements(meetingId, caseNumber);
 
             var videoSync = await GetVideoSync(meetingId, speakingTurns);
 
             return speakingTurns.Select(turn => MapToDTO(turn, videoSync)).ToList();
         }
 
+        public async Task<List<WebApiStatementsDTO>> GetStatementsByPerson(string name, int year)
+        {
+            _logger.LogInformation($"GetStatementsByPerson {name} {year}");
+
+            var statements = await _speakingTurnsRepository.GetSatementsByName(name, year);
+         
+
+            var dtos = new List<WebApiStatementsDTO>();
+            foreach (var statement in statements)
+            {
+                var videoSync = await GetVideoSync(statement.MeetingID, statement);
+                dtos.Add(MapToDTO(statement, videoSync));
+            }
+
+            return dtos;
+        }
+
         private Task<VideoSync?> GetVideoSync(string meetingId, List<Statement> statements)
         {
-            var startTime = statements.OrderBy(turn => turn.Started).FirstOrDefault()?.Started;
+            var statement = statements.OrderBy(turn => turn.Started).FirstOrDefault();
+            return GetVideoSync(meetingId, statement);
+        }
+
+        private Task<VideoSync?> GetVideoSync(string meetingId, Statement? statement)
+        {
+            var startTime = statement?.Started;
             if (startTime == null)
             {
                 return Task.FromResult<VideoSync?>(null);
@@ -50,19 +75,28 @@ namespace Storage.Providers
             return _videoSyncRepository.GetVideoPosition(meetingId, startTime.Value);
         }
 
-        private WebApiStatementsDTO MapToDTO(Statement seat, VideoSync? videoSync)
+        private WebApiStatementsDTO MapToDTO(Statement statement, VideoSync? videoSync)
         {
+            var videoPosition = videoSync.GetVideoPosition(statement.Started);
             var config = new MapperConfiguration(cfg =>
             {
                 cfg.CreateMap<Statement, WebApiStatementsDTO>()
                     .ForMember(dest => dest.StartTime, opt => opt.MapFrom(src => src.Started))
                     .ForMember(dest => dest.EndTime, opt => opt.MapFrom(src => src.Ended))
                     .ForMember(dest => dest.DurationSeconds, opt => opt.MapFrom(src => src.DurationSeconds))
-                    .ForMember(dest => dest.VideoPosition, opt => opt.MapFrom(src => videoSync.GetVideoPosition(src.Started)));
+                    .ForMember(dest => dest.VideoPosition, opt => opt.MapFrom(_ => videoPosition))
+                    .ForMember(dest => dest.VideoLink, opt => opt.MapFrom(src => CreateVideoLink(src, videoPosition)));
             });
             config.AssertConfigurationIsValid();
 
-            return config.CreateMapper().Map<WebApiStatementsDTO>(seat);
+            return config.CreateMapper().Map<WebApiStatementsDTO>(statement);
+        }
+
+        private string CreateVideoLink(Statement statement, int videoPosition)
+        {
+            int year = Int32.Parse(statement.MeetingID.Substring(5, 4));
+            int number = Int32.Parse(statement.MeetingID.Substring(9, 2));
+            return @$"https://helsinkikanava.fi/fi/player/event/view?meeting=kvsto-{year}-{number}#T{videoPosition}";
         }
     }
 }
