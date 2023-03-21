81dc51714f9b96d286fc801480c2de6a0b8d9412
Ville Palo <ville.palo@avanio.com>
2023-03-21 14:37:25 +0200

    Added agendapoint for subitems

--- Storage/Providers/DTOs/WebApiAgendaItemSubItemDTO.cs
+++ Storage/Providers/DTOs/WebApiAgendaItemSubItemDTO.cs
@@ -2,6 +2,8 @@
 {
     public class WebApiAgendaItemSubItemDTO
     {
+        public int AgendaPoint { get; set; }
+
         public string ItemTextFi { get; set; } = string.Empty;
 
         public string ItemNumber { get; set; } = "0";
