c2a06e2297d371bb13367f9849c92b56e2d1879b
Ville Palo <ville.palo@avanio.com>
2023-03-30 13:35:09 +0300

    reservations crealing is now universal

--- Storage/Repositories/StatementsRepository.cs
+++ Storage/Repositories/StatementsRepository.cs
@@ -168,7 +168,6 @@ namespace Storage.Repositories
                 SELECT timestamp 
                 FROM meeting_events 
                 WHERE meeting_id = @meetingId 
-                AND case_number = @agendaPoint
                 AND event_type = '{(int)EventType.ReplyReservationsCleared}'
                 ORDER BY timestamp DESC
                 LIMIT 1";
