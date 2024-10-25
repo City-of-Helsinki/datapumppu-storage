4be0d4f997314e4e6acf21373da21b26a62a4f89
Heikki Riihinen <heikki.riihinen@avanio.com>
2024-10-25 14:13:37 +0300

    Add new endpoint to look for person or daterange from statements

--- Storage/Controllers/StatementController.cs
+++ Storage/Controllers/StatementController.cs
@@ -51,5 +51,34 @@ namespace Storage.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
+
+        [HttpGet("lookup")]
+        public async Task<IActionResult> GetStatementsByPersonOrDate(
+            [FromQuery]List<string> names,
+            [FromQuery]DateTime? startDate,
+            [FromQuery]DateTime? endDate)
+        {
+            try
+            {
+                if ((names == null || !names.Any()) && !startDate.HasValue && !endDate.HasValue)
+                {
+                    return BadRequest(new { Message = "Vähintään yksi hakusuodatin on oltava asetettuna (nimilista (names) tai päivämäärien väli (startDate ja endDate))"});
+                }
+
+                if (endDate.HasValue)
+                {
+                    endDate = endDate.Value.Date.AddDays(1).AddTicks(-1);
+                }
+
+                _logger.LogInformation($"GetStatementsByPersonOrDate {string.Join(", ", names)}, {startDate} {endDate}");
+                var statements = await _statementProvider.GetStatementsByPersonOrDate(names, startDate, endDate);
+                return new OkObjectResult(statements);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "GetStatementsByPersonOrDate failed");
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
     }
 }
--- Storage/Repositories/StatementsRepository.cs
+++ Storage/Repositories/StatementsRepository.cs
@@ -19,6 +19,8 @@ namespace Storage.Repositories
 
         Task<List<Statement>> GetSatementsByName(string name, int year, string lang);
 
+        Task<List<Statement>> GetStatementsByPersonOrDate(List<string> names, DateTime? startDate, DateTime? endDate);
+
         Task<List<StatementReservation>> GetStatementReservations(string meetingId, string agendaPoint);
 
         Task<List<ReplyReservation>> GetReplyReservations(string meetingId, string agendaPoint);
@@ -38,6 +40,50 @@ namespace Storage.Repositories
             _logger = logger;
             _databaseConnectionFactory = databaseConnectionFactory;
         }
+
+    public async Task<List<Statement>> GetStatementsByPersonOrDate(List<string> names, DateTime? startDate, DateTime? endDate)
+    {
+        string? namesString = names != null && names.Any()
+            ? string.Join(",", names.Select(name => name.Trim()))
+            : null;
+
+        var sqlQuery = @"
+            select
+                statements.meeting_id,
+                person,
+                started,
+                ended,
+                speech_type,
+                duration_seconds,
+                additional_info_fi,
+                additional_info_sv,
+                agenda_items.title as title,
+                agenda_items.agenda_point as case_number
+            from
+                statements
+            join
+                meeting_events on statements.event_id = meeting_events.event_id
+            join
+                agenda_items on 
+                    meeting_events.meeting_id = agenda_items.meeting_id and
+                    agenda_items.agenda_point = meeting_events.case_number::int8
+            where
+                (@names IS NULL OR person = ANY(string_to_array(@names, ',')))
+        ";
+
+        // Append date filters conditionally if provided
+        if (startDate.HasValue)
+        {
+            sqlQuery += " AND started >= @startDate";
+        }
+        if (endDate.HasValue)
+        {
+            sqlQuery += " AND ended <= @endDate";
+        }
+
+        using var connection = await _databaseConnectionFactory.CreateOpenConnection();
+        return (await connection.QueryAsync<Statement>(sqlQuery, new { names = namesString, startDate, endDate })).ToList();
+    }
 
         public async Task<List<Statement>> GetSatementsByName(string name, int year, string lang)
         {
